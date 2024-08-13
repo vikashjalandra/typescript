0612b69c369ffc206b9a2fd1838a374efd0ec51e
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-08-13 22:36:45 +0530

    chore: Add myFunction.ts file with functions for adding two numbers and converting a string to uppercase

---
+++ 02Basics/myFunction.ts
@@ -0,0 +1,16 @@
+function addTwo(num:number){
+    return num+2
+}
+
+function getUpper(val:string){
+    return val.toUpperCase()
+}
+
+function signUpUser(name, email, password){
+
+}
+
+addTwo(5)
+getUpper('4vikash')
+
+export{}
