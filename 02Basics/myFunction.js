1df2a46ac1a8df5b6b174e7d5f93a22e8625ddaa
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-08-17 20:59:54 +0530

    feat: Add functions for adding two numbers and converting a string to uppercase

---
+++ 02Basics/myFunction.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    // return "vikash"
+    return 5 + num;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) {
+}
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+var myValue = addTwo(5);
+addTwo(5);
+getUpper('4vikash');
+signUpUser('Vikash', 'v@l', false);
+loginUser("v", "v@");
+// function getValues(myVal: number) {
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200"
+// }
+var getHello = function (s) {
+    return "";
+};
+// const heros=['thor','ironman','spiderman']
+var heros = [1, 2, 3];
+heros.map(function (hero) {
+    return '';
+});
+function consoleError(err) {
+    console.error(err);
+}
--- 02Basics/myFunction.ts
+++ 02Basics/myFunction.ts
@@ -1,16 +1,49 @@
-function addTwo(num:number){
-    return num+2
+function addTwo(num: number): number {
+    // return "vikash"
+    return 5 + num
 }
 
-function getUpper(val:string){
+function getUpper(val: string) {
     return val.toUpperCase()
 }
 
-function signUpUser(name, email, password){
+function signUpUser(name: string, email: string, isPaid: boolean) {
 
 }
 
+const loginUser = (name: string, email: string, isPaid: boolean = false) => { }
+
+let myValue = addTwo(5)
+
 addTwo(5)
 getUpper('4vikash')
+signUpUser('Vikash', 'v@l', false)
+loginUser("v", "v@")
 
-export{}
+
+
+// function getValues(myVal: number) {
+//     if (myVal > 5) {
+//         return true
+
+//     }
+//     return "200"
+// }
+
+const getHello =(s:string):string=>{
+    return ""
+}
+
+// const heros=['thor','ironman','spiderman']
+const heros=[1,2,3]
+
+heros.map((hero):string=>{
+    return ''
+})
+
+
+function consoleError(err:string):void{
+    console.error(err);
+}
+
+export { }
